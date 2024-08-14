d899a44e035760728854d79f87ac486ac9094216
Vitaliy <Alex_halesterin@mail.ru>
2024-08-14 18:12:55 +0300

    dj template urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import include
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,6 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'core/home.html', context={})
